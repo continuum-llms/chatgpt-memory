10a97877f120d1e271fceb487d9e827ad4a556ea
shahrukhx01 <sk28671@gmail.com>
2023-03-14 13:12:01 +0100

    chore: add docstring for `LLMClient`

--- chatgpt_memory/llm_client/llm_client.py
+++ chatgpt_memory/llm_client/llm_client.py
@@ -1,4 +1,11 @@
 class LLMClient:
+    """
+    Wrapper for the HTTP APIs for LLMs acting as data container for API configurations.
+
+    :param api_key: Access key for the remote API.
+    :param time_out: Time in seconds before the request times out.
+    """
+
     def __init__(self, api_key: str, time_out: float = 30):
         self._api_key = api_key
         self._time_out = time_out
