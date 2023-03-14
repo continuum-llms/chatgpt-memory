a90c573cad23782d58f4c03f89e07f092f0ffa67
shahrukhx01 <sk28671@gmail.com>
2023-03-14 20:15:39 +0100

    chore: cleanup datastore config

--- chatgpt_memory/datastore/config.py
+++ chatgpt_memory/datastore/config.py
@@ -1,18 +1,20 @@
 from pydantic import BaseModel
 from enum import Enum
 
+
 class RedisIndexType(Enum):
     hnsw = "HNSW"
     flat = "FLAT"
+
 
 class DataStoreConfig(BaseModel):
     host: str
     port: int
     password: str
 
+
 class RedisDataStoreConfig(DataStoreConfig):
     index_type: str = RedisIndexType.hnsw.value
-    vector_field_name: str 
+    vector_field_name: str
     vector_dimensions: int
-    distance_metric: = "L2"
-    {"TYPE": "FLOAT32", "DIM": vector_dimensions, "DISTANCE_METRIC": distance_metric, "INITIAL_CAP": number_of_vectors, "BLOCK_SIZE":number_of_vectors }
+    distance_metric: str = "L2"
