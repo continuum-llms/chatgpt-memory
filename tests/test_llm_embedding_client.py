0d6601b5eae7ceede05f44a17999e295dc6d9b86
shahrukhx01 <sk28671@gmail.com>
2023-03-14 14:58:33 +0100

    refactor: refine unit test for embedding client

--- tests/test_llm_embedding_client.py
+++ tests/test_llm_embedding_client.py
@@ -5,8 +5,16 @@ from chatgpt_memory.llm_client.openai.embedding.embedding_client import (
 SAMPLE_QUERIES = ["Where is Berlin?"]
 SAMPLE_DOCUMENTS = [{"text": "Berlin is located in Germany."}]
 
+EXPECTED_EMBEDDING_DIMENSIONS = (1, 1024)
+
 
 def test_openai_embedding_client(openai_embedding_client: OpenAIEmbeddingClient):
-    assert openai_embedding_client.embed_queries(SAMPLE_QUERIES).shape[0]
+    assert (
+        openai_embedding_client.embed_queries(SAMPLE_QUERIES).shape
+        == EXPECTED_EMBEDDING_DIMENSIONS
+    ), "Generated query embedding is of inconsistent dimension"
 
-    assert openai_embedding_client.embed_documents(SAMPLE_DOCUMENTS).shape[0]
+    assert (
+        openai_embedding_client.embed_documents(SAMPLE_DOCUMENTS).shape
+        == EXPECTED_EMBEDDING_DIMENSIONS
+    ), "Generated document(s) embedding is of inconsistent dimension"
