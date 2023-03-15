7ebf6939188464fe7ade4a2d32234d7508e7c322
Navdeeppal Singh <s8nlsing@stud.uni-saarland.de>
2023-03-15 22:22:27 +0100

    feat: add memory manager tests

---
+++ tests/test_memory_manager.py
@@ -0,0 +1,60 @@
+from chatgpt_memory.datastore.config import RedisDataStoreConfig
+from chatgpt_memory.datastore.redis import RedisDataStore
+from chatgpt_memory.environment import OPENAI_API_KEY
+from chatgpt_memory.llm_client.openai.embedding.config import EmbeddingConfig
+from chatgpt_memory.llm_client.openai.embedding.embedding_client import EmbeddingClient
+from chatgpt_memory.memory.manager import MemoryManager
+from chatgpt_memory.memory.memory import Memory
+
+
+class TestMemoryManager:
+    def setup(self):
+        # create a redis datastore
+        redis_datastore_config = RedisDataStoreConfig()
+        self.datastore = RedisDataStore(redis_datastore_config)
+
+        # create an openai embedding client
+        embedding_client_config = EmbeddingConfig(api_key=OPENAI_API_KEY)
+        self.embedding_client = EmbeddingClient(embedding_client_config)
+
+    def test_conversation_insertion_and_deletion(self):
+        # create a memory manager
+        memory_manager = MemoryManager(datastore=self.datastore, embed_client=self.embedding_client)
+
+        # assert that the memory manager is initially empty
+        assert len(memory_manager.conversations) == 0
+
+        # add a conversation to the memory manager
+        memory_manager.add_conversation(Memory(conversation_id="1"))
+
+        # assert that the memory manager has 1 conversation
+        assert len(memory_manager.conversations) == 1
+
+        # remove the conversation from the memory manager
+        memory_manager.remove_conversation(Memory(conversation_id="1"))
+
+        # assert that the memory manager is empty
+        assert len(memory_manager.conversations) == 0
+
+    def test_adding_messages_to_conversation(self):
+        # create a memory manager
+        memory_manager = MemoryManager(datastore=self.datastore, embed_client=self.embedding_client)
+
+        # add a conversation to the memory manager
+        memory_manager.add_conversation(Memory(conversation_id="1"))
+
+        # assert that the memory manager has 1 conversation
+        assert len(memory_manager.conversations) == 1
+
+        # add a message to the conversation
+        memory_manager.add_message(conversation_id="1", user="Hello", system="Hello. How are you?")
+
+        # get messages for that conversation
+        messages = memory_manager.get_messages(conversation_id="1", query="Hello")
+
+        # assert that the message was added
+        assert len(messages) == 1
+
+        # assert that the message is correct
+        assert messages[0]["text"] == "user: Hello\nsystem: Hello. How are you?"
+        assert messages[0]["conversation_id"] == "1"
